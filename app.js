7286880e96cd927b7b216eabf1f7d2b462341245
Palo <pjestrada006@hotmail.com>
2017-10-05 21:24:22 -0600

    pinger

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const request = require('request');
+var shell = require('shelljs');
+const app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
 
 app.listen(5002, function () {
     console.log('Example app listening on port 5002!');
 
 
-})
+});
 
 function makRequest(){
     request('127.0.0.1:4000', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        if(error){
+            console.log("Smart Site Died: I will respawn it haha... ");
+            shell.exec('./ docker-reset.sh')
+        }
+        else{
+
+        }
     });
 }
 
 (function() {
     var c = 0;
     var timeout = setInterval(function() {
-        //do thing
+        makRequest();
         c++;
         if (c > 2) {
             clearInterval(timeout);
